760adc0a5737cdd58e2f36376922f755ebc96d1f
wykr0 <manuel.cruces@sansano.usm.cl>
2024-05-08 19:35:26 -0400

    manejo de errores

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -7,11 +7,26 @@ exports.getAllUsers = (ctx) =>{
 }
 
 exports.getUser = (ctx) => {
-    ctx.body = userActions.getUser(Number(ctx.params.id))
-    return ctx
+    const user = userActions.getUser(Number(ctx.params.id))
+    if (user.length!==0) {
+        ctx.body = user
+        return ctx
+    } else {
+        ctx.status = 404
+        ctx.body = {
+            message: "Usuario no encontrado"
+        }
+    }
+    
 }
 
 exports.createUser = (ctx) => {
+    const body = ctx.request.body
+    if (!body || Object.keys(body).length === 0) {
+        ctx.status = 400
+        ctx.body = { message: 'User data is missing' }
+        return ctx
+    }
     userActions.addUser(ctx.request.body)
     ctx.body = {status: 200,
         message: 'User was created'
