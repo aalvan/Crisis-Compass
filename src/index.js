7a659ec4e23bb946c37986aaf58971163ec9b071
Alexis Alva <alexis.alva@sansano.usm.cl>
2024-07-07 22:47:25 -0400

    Add cors library to avoid error 'Access-Control-Allow-Origin' in the browser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import koa from 'koa'
-import bodyParser from 'koa-body'
-import router from './routes/index'
+import Koa from 'koa';
+import bodyParser from 'koa-body';
+import cors from '@koa/cors';
+import router from './routes/index';
 
-const app = new koa()
-const port = 3001
+const app = new Koa();
+const port = 3001;
 
-app.use(bodyParser({ multipart: true, urlencoded: true }))
-app.use(router.routes())
+app.use(cors()); // Enable CORS for all routes
+app.use(bodyParser({ multipart: true, urlencoded: true }));
+app.use(router.routes());
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
